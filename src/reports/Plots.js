641811d7731b8cca0db8a8fb16bff6ee4c65775e
Chad Altemose <caltemose@hugeinc.com>
2017-05-03 16:11:22 -0400

    adding Plots component to support multiple traces in one plot

---
+++ src/reports/Plots.js
@@ -0,0 +1,45 @@
+/* global Plotly */
+import React, { Component } from 'react'
+
+class Plots extends Component {
+    componentDidMount () {
+        // Currently this condition is never met
+        // if (this.props.xData.length && this.props.yData.length) {
+        //     var title = this.props.title || ''
+        //     Plotly.newPlot(this.node, [{
+        //         x: this.props.xData,
+        //         y: this.props.yData,
+        //         type: 'scatter',
+        //         line: {
+        //             color: this.props.color
+        //         }
+        //     }], {title})
+        // }
+    }
+
+    componentDidUpdate () {
+        // Plotly.update() doesn't seem to work in this usage.
+        // Need to determine a more efficient way of updating
+        // the data rather than replotting the entire graph
+        // every single time.
+        // if (this.props.xData.length && this.props.yData.length) {
+        //     var title = this.props.title || ''
+        //     Plotly.newPlot(this.node, [{
+        //         x: this.props.xData,
+        //         y: this.props.yData,
+        //         type: 'scatter',
+        //         line: {
+        //             color: this.props.color
+        //         }
+        //     }], {title})
+        // }
+    }
+
+    render () {
+        return (
+            <div ref={(node) => { this.node = node }}></div>
+        )
+    }
+}
+
+export default Plots
